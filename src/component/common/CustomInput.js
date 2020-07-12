01fc01d08c55753966245501890d0e71cc60023b
Kevin Berthelot <kevinbradleyberthelot@gmail.com>
2020-07-12 19:21:26 +0530

    changed custom input and added Formik

--- src/component/common/CustomInput.js
+++ src/component/common/CustomInput.js
@@ -3,11 +3,23 @@ import { useField } from "formik";
 import "./customInput.css";
 
 const CustomInput = (props) => {
-  //const [field, meta] = useField(props);
+  const [field, meta] = useField(props);
+  //console.log("field", field);
+  console.log("meta", meta);
   return (
     <>
       <div className="input-group">
-        <input {...props} />
+        <input
+          name={field.name}
+          value={field.value || ""}
+          onChange={field.onChange}
+          {...props}
+        />
+        {meta.error && meta.touched && (
+          <div>
+            <h1>{meta.error}</h1>
+          </div>
+        )}
       </div>
     </>
   );
--- src/component/signup_form/FormCreator.js
+++ src/component/signup_form/FormCreator.js
@@ -1,11 +1,27 @@
 import React from "react";
-import Formik from "formik";
+import { Formik } from "formik";
 
 const FormCreator = (props) => {
   const componentArray = React.Children.toArray(props.children);
   const step = componentArray[0];
+  //console.log(step.props.validationSchema);
 
-  return <>{step}</>;
+  return (
+    <>
+      <Formik
+        initialValues={step.props.initValues}
+        validationSchema={step.props.validationSchema}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            actions.setSubmitting(false);
+          }, 1000);
+        }}
+      >
+        {(props) => <form onSubmit={props.handleSubmit}>{step}</form>}
+      </Formik>
+    </>
+  );
 };
 
 export default FormCreator;
